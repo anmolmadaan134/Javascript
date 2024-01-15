b0fc1c4c164b191928fcd9aa9584f238e89cba73
anmolmadaan134 <156285112+anmolmadaan134@users.noreply.github.com>
2024-01-15 08:25:11 +0000

    operations

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -42,3 +42,11 @@ console.log(1 + 2 + "2");
 
 console.log(+true);
 console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
