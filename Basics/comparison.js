4e0c97d69bdf67144a22d7b1d7981f179148b7bd
anmolmadaan134 <156285112+anmolmadaan134@users.noreply.github.com>
2024-01-15 09:14:17 +0000

    Comparison and dataTypes Summary

---
+++ Basics/comparison.js
@@ -0,0 +1,16 @@
+//console.log("2">1);
+//console.log("02">1);
+
+//console.log(null>0)
+//console.log(null==0)
+//console.log(null>=0) // Comparison convert null to a number, treating it as a 0.
+
+//console.log(undefined>0)
+//console.log(undefined==0)
+//console.log(undefined>=0)
+
+// === It equates value as well as data type of variable.
+
+console.log("2"===2)
+console.log(2===2)
+
---
+++ Basics/dataTypes_summary.js
@@ -0,0 +1,52 @@
+// Primitive
+
+// 7 types - String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 200
+const scoreV = 209.2
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id)
+//console.log(anotherId);
+
+console.log(id===anotherId);
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+const cricketers = ["Rohit","Dhoni","Virat"]
+
+let myObj = {
+    name: Anmol,
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// All dataypes of Non-primtive are known as functions. For function it known as function object
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
