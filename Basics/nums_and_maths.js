07a5501236c4ac5d1d05068002f363d8b0f65d38
anmolmadaan134 <156285112+anmolmadaan134@users.noreply.github.com>
2024-01-16 15:40:32 +0000

    Maths

---
+++ Basics/nums_and_maths.js
@@ -0,0 +1,34 @@
+//const score = 400
+//console.log(score);
+
+//const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString())
+//console.log(balance.toFixed(2));
+
+//const otherNumber = 23.98984
+
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN')); // converts zeros into a us format by default for ind format use 'en-IN'
+
+//************************* Maths *************************
+
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+
+
+console.log(Math.random()); // random is a func which gives value between 0 and 1 always.
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const max=20
+const min=10
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
