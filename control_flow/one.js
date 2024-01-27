5fb545fe41f663075c408c978d883184c7750451
anmolmadaan134 <156285112+anmolmadaan134@users.noreply.github.com>
2024-01-27 14:52:45 +0000

    control flow

---
+++ control_flow/one.js
@@ -0,0 +1,13 @@
+//if
+
+const score = 200
+
+if(score>100){
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
+
+const balance = 1000
+
+// if(balance>500) console.log("test"); console.log("test2");
+
