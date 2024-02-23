946c40a1cfed112a5f755d24582d49e001f74d4b
anmolmadaan134 <156285112+anmolmadaan134@users.noreply.github.com>
2024-02-23 15:54:02 +0000

    objects

---
+++ Classes_and_objects/mathpi.js
@@ -0,0 +1,32 @@
+// const descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+
+// console.log(descriptor);
+
+const chai = {
+    name: 'ginger tea',
+    price: 250,
+    isAvailable: true,
+
+    orderChai :function(){
+        console.log("Tea destroyed");
+    }
+
+
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"price"));
+
+// Object.defineProperty(chai,'name',{
+//     writable:false,
+//     enumerable:false,
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+for (let [key,value] of Object.entries(chai)){
+
+    if(typeof value !== 'function'){
+        console.log(`${key}: ${value}`);
+    }
+    
+}
